9f8570f0dc60de4465fe012057cba973091789e8
Jasmine Alarcon <jasminealarcon@Jasmines-MacBook-Air-2.local>
2025-10-14 07:40:54 -0400

    function_import

--- azure_func/function_app.py
+++ azure_func/function_app.py
@@ -6,3 +6,10 @@ app = func.FunctionApp()
 @app.route(route="ping", methods=[func.HttpMethod.GET], auth_level=func.AuthLevel.ANONYMOUS)
 def ping(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse("pong", status_code=200, mimetype="text/plain")
+
+import pipeline_t100  # --- IGNORE ---
+import count_rowst100  # --- IGNORE ---
+import manifest_t100  # --- IGNORE ---
+import db1b_timer  # --- IGNORE ---
+import download_t100  # --- IGNORE ---
+import list_t100  # --- IGNORE ---
