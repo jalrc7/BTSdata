1aa3f9f9a1d91ed6caabd05451ef87e8a98ae3f8
Jasmine Alarcon <jasminealarcon@Jasmines-MacBook-Air-2.local>
2025-10-14 04:10:40 -0400

    Wire up Functions v2 app and fix deploy

--- azure_func/function_app.py
+++ azure_func/function_app.py
@@ -2,12 +2,6 @@ import azure.functions as func
 
 app = func.FunctionApp()
 
-import pipeline_t100 
-import count_rowst100 
-import manifest_t100
-import db1b_timer
-import download_t100
-import list_t100
 
 @app.route(route="ping", methods=[func.HttpMethod.GET], auth_level=func.AuthLevel.ANONYMOUS)
 def ping(req: func.HttpRequest) -> func.HttpResponse:
