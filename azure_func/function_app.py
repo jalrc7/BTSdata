78ae0a0fe26921a659b949a05cb654f22e523e19
Jasmine Alarcon <jalrc@bu.edu>
2025-10-15 11:38:51 -0400

    Use package-relative imports across function app

--- azure_func/function_app.py
+++ azure_func/function_app.py
@@ -3,12 +3,10 @@ import azure.functions as func
 app = func.FunctionApp()
 
 
-@app.route(route="ping", methods=[func.HttpMethod.GET], auth_level=func.AuthLevel.ANONYMOUS)
-def ping(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("pong", status_code=200, mimetype="text/plain")
+@app.route(route="health", methods=[func.HttpMethod.GET], auth_level=func.AuthLevel.ANONYMOUS)
+def health(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse("ok", status_code=200, mimetype="text/plain")
 
 #import pipeline_t100  # --- IGNORE ---
 #import manifest_t100  # --- IGNORE ---
 #import db1b_timer  # --- IGNORE ---
-
-from . import  download_t100
