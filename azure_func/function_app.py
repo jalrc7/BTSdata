4e15bd0754d3d3774be6fc0e656248cf1b36126e
Jasmine Alarcon <jalrc@bu.edu>
2025-10-15 11:50:18 -0400

    Minimal boot: only health route; ignore all other modules

--- azure_func/function_app.py
+++ azure_func/function_app.py
@@ -1,12 +1,6 @@
 import azure.functions as func
-
 app = func.FunctionApp()
-
 
 @app.route(route="health", methods=[func.HttpMethod.GET], auth_level=func.AuthLevel.ANONYMOUS)
 def health(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse("ok", status_code=200, mimetype="text/plain")
-
-#import pipeline_t100  # --- IGNORE ---
-#import manifest_t100  # --- IGNORE ---
-#import db1b_timer  # --- IGNORE ---
