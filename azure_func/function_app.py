5c8966defff7b534d2390406e27dd96aff0fafe7
Jasmine Alarcon <jalrc@bu.edu>
2025-10-15 12:39:51 -0400

    Add V1 Health function and remove V2 function_app.py

--- azure_func/function_app.py
+++
@@ -1,6 +0,0 @@
-import azure.functions as func
-app = func.FunctionApp()
-
-@app.route(route="health", methods=[func.HttpMethod.GET], auth_level=func.AuthLevel.ANONYMOUS)
-def health(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("ok", status_code=200, mimetype="text/plain")
